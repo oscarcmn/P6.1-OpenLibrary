fd95af0706a8cf8d54caf023c4cdbf64a5b9d998
ocarmena <oscarcarmenaprofesional@gmail.com>
2024-11-29 13:07:09 +0100

    y el cuerpo lo sabe

--- libraryAPI.js
+++ libraryAPI.js
@@ -1,24 +1,9 @@
 const API_URL = "https://openlibrary.org";
 
 export async function getBooksByTitle(title) {
-  const formatTitle = title.replace(/ /g, "+");
   try {
     const response = await fetch(
-      `${API_URL}/search.json?title=${formatTitle}&fields=title,author_name,publish_year,ratings_average,ratings_count,cover_i,number_of_pages_median&limit=1`
-    );
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error:", error);
-    return null;
-  }
-}
-
-export async function getBooksByTitle(title) {
-  const formattedTitle = title.replace(/ /g, "+");
-  try {
-    const response = await fetch(
-      `${API_URL}/search.json?title=${formattedTitle}&fields=title,author_name,publish_year,ratings_average,ratings_count,cover_i,number_of_pages_median&language=eng`
+      `${API_URL}/search.json?title=the+way+of+kings&fields=title,author_name,publish_year,ratings_average,ratings_count,cover_i,number_of_pages_median&language=eng`
     );
     const data = await response.json();
     return data;
---
+++ main.js
@@ -0,0 +1,55 @@
+import { getBooksByTitle } from "./libraryAPI.js";
+
+document.addEventListener("DOMContentLoaded", (ev) => {
+  const postSection = document.querySelector(".posts");
+
+  getBooksByTitle()
+    .then((books) => {
+      console.log(books);
+      renderBooks(books);
+    })
+    .catch((err) => {
+      console.log("ERROR: " + err);
+      return null;
+    });
+
+  function renderBooks(books) {
+    const card = document.querySelector("#cardpreview");
+    books.docs.forEach((book) => {
+      const cardItem = document.createElement("div");
+
+      cardItem.classList.add("card-item");
+      cardItem.innerHTML = `
+        <div class="card mx-auto font" style="width: 18rem;">
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}.jpg" class="img-top" alt="Imagen de ejemplo">
+                <div class="card-body ">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="title">${book.title}</h5>
+                        <div>
+                            <p class="year">${book.publish_year}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="property">author</p>
+                        <p class="dataproperty">${book.author_name}</p>
+                    </div>
+                    <div>
+                        <p class="property">avg rating</p>
+                        <p class="dataproperty">${book.ratings_average}</p>
+                    </div>
+                    <div>
+                        <p class="property">number of ratings</p>
+                        <p class="dataproperty">${book.ratings_count}</p>
+                    </div>
+                    <div>
+                        <p class="property">number of pages</p>
+                        <p class="dataproperty">${book.number_of_pages_median}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+      card.appendChild(cardItem);
+    });
+  }
+  getBooksByTitle();
+});
